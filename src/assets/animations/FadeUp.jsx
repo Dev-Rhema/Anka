612ca28f2830a3242bacbde62a26af4b8ee32b0c
Rhema <reymarmcoc@gmail.com>
2025-08-05 13:23:22 +0100

    smooth

--- src/assets/animations/FadeUp.jsx
+++ src/assets/animations/FadeUp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, spring } from "framer-motion";
 
 function FadeUp({ Children, className }) {
   return (
@@ -8,7 +8,7 @@ function FadeUp({ Children, className }) {
         initial={{ opacity: 0, translateY: "50%" }}
         whileInView={{ opacity: 1, translateY: 0 }}
         viewport={{ once: true }}
-        transition={{ duration: 1, ease: "easeInOut" }}
+        transition={{ duration: 2, ease: [0.25, 0.8, 0.25, 1], type: spring }}
         className={className}
       >
         {Children}
